6e74bd41cc5b9a88e5ba21ad6eaea11538df520c
Adi <adirosenthal540@gmail.com>
2020-09-24 19:53:59 +0300

    update data

--- test_images/prepare_images_for_training.py
+++ test_images/prepare_images_for_training.py
@@ -1,9 +1,14 @@
 from PIL import Image
-
+import os
 def convert_image_format(image_path):
     im = Image.open(image_path)
     format = im.format
     sizeFormat = len(format)
     if format!="TIFF":
         im.save(image_path[:(-1*sizeFormat)]+".tiff", 'TIFF')
+    os.remove(image_path)
 
+for i in range(1, 12):
+    path = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+"..tiff"
+    path2 = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+".tiff"
+    os.rename(path, path2)
