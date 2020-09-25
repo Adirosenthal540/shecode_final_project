a4940308be68b667ddac046f6d5d4388ce4b2c8f
Adi <adirosenthal540@gmail.com>
2020-09-25 18:34:43 +0300

    update data

--- test_images/prepare_images_for_training.py
+++ test_images/prepare_images_for_training.py
@@ -4,11 +4,11 @@ def convert_image_format(image_path):
     im = Image.open(image_path)
     format = im.format
     sizeFormat = len(format)
-    if format!="TIFF":
-        im.save(image_path[:(-1*sizeFormat)]+".tiff", 'TIFF')
+    if format!="TIF":
+        im.save(image_path[:(-1*sizeFormat)]+".tif", 'TIF')
     os.remove(image_path)
 
 for i in range(1, 12):
-    path = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+"..tiff"
-    path2 = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+".tiff"
+    path = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+".tiff"
+    path2 = "C:\\Users\\Adi Rosental\\Documents\\she_code\\shecode_final_project\\handwriteDoc\\test_train_hebrew\\rows\\" + str(i)+".tif"
     os.rename(path, path2)
